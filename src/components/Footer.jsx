b64fb774dfcb52053b335fc2a577e4d5033994f5
hsinJee <sumhsinjee@gmail.com>
2026-02-22 18:35:09 +0000

    fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer style={{ padding: '1rem', textAlign: 'center', marginTop: '2rem', borderTop: '1px solid #ccc' }}>
-      <p>Designed and coded in <a href="https://code.visualstudio.com/">Visual Studio Code</a> by yours truly, built with Vite and deplyed with "filler"</p>
+      <p>Designed and coded in <a href="https://code.visualstudio.com/">Visual Studio Code</a> by yours truly, built with Vite and deplyed with Vercel</p>
     </footer>
   );
 }
