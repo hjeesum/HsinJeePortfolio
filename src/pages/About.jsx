76a2f1397dc566b6803b21ea57ff11b80fa08b63
hsinJee <sumhsinjee@gmail.com>
2026-02-22 19:14:34 +0000

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,21 +2,17 @@ function About() {
   return (
     <div className="mb-16">
       <p className="mb-4">
-        Hii! I'm a software engineer with a focus on AI and machine learning,
+        Hi! I'm a software engineer with a focus on AI and machine learning,
         currently completing an integrated master's in Computer Science with
         Artificial Intelligence at the University of Leeds (graduating June
         2026).
       </p>
       <p className="mb-4">
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release
-        of Letraset sheets containing Lorem Ipsum passages, and more recently
-        with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum
+        I love myself a good challenge, and am always looking for opportunities
+        to learn and grow. I'm passionate about using technology to solve
+        real-world problems, and am always looking for ways to make a positive
+        impact on the world. I'm also a big fan of open source software, and am
+        always looking for ways to contribute to the community.
       </p>
       <p className="mb-4">
         Lorem Ipsum is simply dummy text of the printing and typesetting
