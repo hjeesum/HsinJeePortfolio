cdb4b7a47fcd5f8e3a40fcb13ed3ee94044dcbfd
hsinJee <sumhsinjee@gmail.com>
2026-02-24 19:28:49 +0000

    Added more info

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -1,22 +1,42 @@
 import RedirectBtn from "./RedirectBtn";
+import { useState } from "react";
+import { useEffect } from "react";
 
 export default function ProjectItem({
   project,
   technologies,
   description,
   link,
+  images,
 }) {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  useEffect(() => {
+    if (images.length <= 1) return;
+
+    const interval = setInterval(() => {
+      setCurrentIndex((prev) => (prev + 1) % images.length);
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, [images]);
   return (
     <a
       href={link}
       className="relative -mx-4 md:-mx-5 rounded-xl hover:shadow-lg duration-200 hover:bg-black/30 hover:opacity-100 group-hover:opacity-60 transition-colors cursor-pointer"
     >
       <div className="max-w-4xl mx-auto grid grid-cols-1 md:grid-cols-12 gap-2 md:gap-4 p-2.5 md:p-7">
-        <img
-          src="/projectimage.avif"
-          className="md:col-span-3 col-span-12 w-full h-auto rounded-md mt-2"
-          alt="Project image"
-        />
+        <div className="relative md:col-span-3 col-span-12 mt-2 w-[131px] h-[100px]">
+  {images.map((img, index) => (
+    <img
+      key={index}
+      src={`/${img}`}
+      alt={`Project image ${index}`}
+      className={`absolute top-0 left-0 w-full h-full object-fill rounded-md transition-opacity duration-700 ${
+        index === currentIndex ? "opacity-100 z-10" : "opacity-0 z-0"
+      }`}
+    />
+  ))}
+</div>
         <div className="md:col-span-9 col-span-12">
           <RedirectBtn>{project}</RedirectBtn>
           <p className="text-[14px] mt-2">{description}</p>
--- src/pages/ListProject.jsx
+++ src/pages/ListProject.jsx
@@ -4,36 +4,40 @@ export default function ListProject() {
   return (
     <div className="group flex flex-col gap-4 mb-6">
       <ProjectItem
-        project={"AI App"}
-        technologies={["React", "Node.js", "Express", "MongoDB"]}
+        project={"Cohere RAG app for research paper"}
+        technologies={["Cohere API", "Python", "Docker", "GROBID"]}
         description={
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
         }
-        link={""}
+        link={"https://github.com/hjeesum/ResearchPaperSystem"}
+        images={["cohere1.jpg", "cohere2.png", "cohere3.png"]}
       />
       <ProjectItem
-        project={"AI App"}
-        technologies={["React", "Node.js", "Express", "MongoDB"]}
+        project={"Hsin Jee Portfolio"}
+        technologies={["React", "Vite", "HTML/CSS", "JavaScript"]}
         description={
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
         }
-        link={""}
+        link={"https://github.com/hjeesum/HsinJeePortfolio"}
+        images={["portfolio1.gif"]}
       />
       <ProjectItem
-        project={"AI App"}
-        technologies={["React", "Node.js", "Express", "MongoDB"]}
+        project={"Custom CNN model for Breast Cancer Histopathology Images"}
+        technologies={["TensorFlow", "Python", "PyTorch", "NumPy"]}
         description={
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+          "Developed custom Convolutional Neural Network from scratch in pure NumPy for breast cancer tumor detection from histopathological images. Outperformed existing TensorFlow CNNs such as VGG16 and ResNet50, achieving a final result of 90% model accuracy while reducing the model size by 40x."
         }
-        link={""}
+        link={"https://github.com/hjeesum/BreastCancerCNN"}
+        images={["CNN1.png", "CNN2.png", "CNN3.png", "CNN4.png", "CNN5.png"]}
       />
       <ProjectItem
-        project={"AI App"}
-        technologies={["React", "Node.js", "Express", "MongoDB"]}
+        project={"Formula Studnet AI"}
+        technologies={["ROS2", "C", "C++", "MongoDB"]}
         description={
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
         }
-        link={""}
+        link={"https://github.com/GryphonRacingAI/gra-ros2"}
+        images={["projectimage.avif"]}
       />
     </div>
   );
